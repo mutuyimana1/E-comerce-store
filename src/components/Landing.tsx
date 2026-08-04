85d576ce1ea377bd216ac46a3c19604f11cd093a
unknown <amutuyimana64@gmail.com>
2026-08-04 12:03:44 +0200

    fx build

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { ArrowRight, CheckCircle ,Bed,Apple,Laptop} from 'lucide-react';
 import { Link } from 'react-router-dom';
